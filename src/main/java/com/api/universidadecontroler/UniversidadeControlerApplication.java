63aa19315ca369cc525c136ffed269938d36d34e
lvd <lvd@certi.org.br>
2024-02-27 22:00:58 -0300

    meu primeiro commit com spring

---
+++ src/main/java/com/api/universidadecontroler/UniversidadeControlerApplication.java
@@ -0,0 +1,13 @@
+package com.api.universidadecontroler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadeControlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversidadeControlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/universidadecontroler/UniversidadeControlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.universidadecontroler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadeControlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

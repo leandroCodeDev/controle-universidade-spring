6849200177de137b9489fe3653ca6ddb22e82857
lvd <lvd@certi.org.br>
2024-02-28 20:27:16 -0300

    criado model Aluno

---
+++ src/main/java/com/api/universidadecontroler/models/Aluno.java
@@ -0,0 +1,38 @@
+package com.api.universidadecontroler.models;
+
+import java.util.Date;
+public class Aluno {
+
+    private static int contador = 1;
+    private int id;
+    private String nome;
+    private Date dataNascimento;
+
+    private static int  gerarIdAluno(){
+        return contador++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date data_nascimento) {
+        this.dataNascimento = data_nascimento;
+    }
+}
